d42b14ccddcbd393bf2b8bd5f91badf558c85770
Lesha07 <simchuklesha@gmail.com>
2024-10-24 03:04:28 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Number first:");
+int firstNubmer = Int32.Parse(Console.ReadLine());
+
+int secondNubmer = 0;
+
+try
+{
+    int result = firstNubmer / secondNubmer;
+    Console.WriteLine(result);
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    Console.WriteLine("Division by zero is impossible!");
+}
